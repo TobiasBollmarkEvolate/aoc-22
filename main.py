1be53f56cbf21a09d4c8b11ecbbec1db2f1ef0e5
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-01 15:32:09 +0100

    Day 1

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
-def dec1_part1(data):
-    highest = 0
-    count = 0
-    for item in data:
-        if not item:
+def dec1_part1(lines: list[str]) -> int:
+    highest: int = 0
+    count: int = 0
+    for line in lines:
+        if not line:
             if count > highest:
                 highest = count
             count = 0
         else:
-            count += int(item)
+            count += int(line)
     return highest
 
 
-def dec1_part2(data):
-    sums = []
-    count = 0
-    for item in data:
-        if not item:
+def dec1_part2(lines: list[str]) -> int:
+    sums: list[int] = []
+    count: int = 0
+    for line in lines:
+        if not line:
             sums.append(count)
             count = 0
         else:
-            count += int(item)
+            count += int(line)
     sums.sort()
     return sum(sums[-3:])
 
 
 if __name__ == '__main__':
-    # 1
     with open('input.txt') as f:
-        lines = f.read().splitlines()
-    result1 = dec1_part1(lines)
+        file_lines: list[str] = f.read().splitlines()
+    result1: int = dec1_part1(file_lines)
     print(result1)
-    result2 = dec1_part2(lines)
+    result2: int = dec1_part2(file_lines)
     print(result2)
-
-    # 2
-

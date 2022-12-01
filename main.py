046e0e9c23b805223d8ac70a58f3366c7b97070e
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-01 15:50:39 +0100

    Day 1 Optimized

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-def dec1_part1(lines: list[str]) -> int:
-    highest: int = 0
-    count: int = 0
-    for line in lines:
-        if not line:
-            if count > highest:
-                highest = count
-            count = 0
-        else:
-            count += int(line)
-    return highest
+from typing import TextIO
 
 
-def dec1_part2(lines: list[str]) -> int:
+def dec1(f: TextIO) -> tuple[int, int]:
     sums: list[int] = []
     count: int = 0
-    for line in lines:
-        if not line:
+    for line_number, line in enumerate(f):
+        if line == '\n':
             sums.append(count)
             count = 0
         else:
             count += int(line)
     sums.sort()
-    return sum(sums[-3:])
+    return sums[-1:][0], sum(sums[-3:])
 
 
 if __name__ == '__main__':
-    with open('input.txt') as f:
-        file_lines: list[str] = f.read().splitlines()
-    result1: int = dec1_part1(file_lines)
-    print(result1)
-    result2: int = dec1_part2(file_lines)
-    print(result2)
+    with open('input.txt') as fp:
+        print(dec1(fp))

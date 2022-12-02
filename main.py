6c7dd5f3306b325f71dbaf10d7364d96c314d94f
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-02 09:29:48 +0100

    Day 2 - Part1

--- main.py
+++ main.py
@@ -13,5 +13,42 @@ def dec1(file_name: str) -> tuple[int, int]:
         return sums[-1:][0], sum(sums)
 
 
+def dec2(file_name: str) -> int:
+    player_points = {
+        "X": 1,
+        "Y": 2,
+        "Z": 3
+    }
+
+    def points(p1: str, p2: str):
+        if p1 == "A":  # Rock
+            if p2 == "Y":  # Paper
+                return 6
+            elif p2 == "Z":  # Scissors
+                return 0
+        elif p1 == "B":  # Paper
+            if p2 == "X":  # Rock
+                return 0
+            elif p2 == "Z":  # Scissors
+                return 6
+        elif p1 == "C":  # Scissors
+            if p2 == "X":  # Rock
+                return 6
+            elif p2 == "Y":  # Paper
+                return 0
+        return 3
+
+    def hand(i):
+        pass
+
+    score: int = 0
+    with open(file_name) as f:
+        for line in f:
+            opponent, player = line.split()
+            score += points(opponent, player) + player_points[player]
+    return score
+
+
 if __name__ == '__main__':
-    print(dec1('input.txt'))
+    print(dec1('input1.txt'))
+    print(dec2('input2.txt'))

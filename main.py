ef71546852b885bfb7b1b035a7fcd6da7f3053be
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-02 10:08:59 +0100

    Day 2 - Part2

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ def dec1(file_name: str) -> tuple[int, int]:
         return sums[-1:][0], sum(sums)
 
 
-def dec2(file_name: str) -> int:
+def dec2(file_name: str) -> tuple[int, int]:
     player_points = {
-        "X": 1,
-        "Y": 2,
-        "Z": 3
+        "rock": 1,
+        "paper": 2,
+        "scissors": 3
     }
     hand = {
         "A": "rock",
@@ -27,11 +27,17 @@ def dec2(file_name: str) -> int:
         "Y": "paper",
         "Z": "scissors"
     }
+    strat = {
+        "X": "loose",
+        "Y": "draw",
+        "Z": "win"
+    }
     beat = {
         "rock": "scissors",
         "paper": "rock",
         "scissors": "paper"
     }
+    loose = {v: k for k, v in beat.items()}  # Creates a reversed "beat"
 
     def beats(h1, h2):
         if h1 == h2:
@@ -40,17 +46,23 @@ def dec2(file_name: str) -> int:
             return 0
         return 6
 
-    def points_part1(p1: str, p2: str):
-        p1 = hand[p1]
-        p2 = hand[p2]
-        return beats(p1, p2)
+    def points_part2(p, s):
+        p = hand[p]
+        s = strat[s]
+        if s == "draw":
+            return 3 + player_points[p]
+        elif s == "win":
+            return 6 + player_points[loose[p]]
+        return 0 + player_points[beat[p]]  # loose
 
     score_part1: int = 0
+    score_part2: int = 0
     with open(file_name) as f:
         for line in f:
             a, x = line.split()
-            score_part1 += points_part1(a, x) + player_points[x]
-    return score_part1
+            score_part1 += beats(hand[a], hand[x]) + player_points[hand[x]]
+            score_part2 += points_part2(a, x)
+    return score_part1, score_part2
 
 
 if __name__ == '__main__':

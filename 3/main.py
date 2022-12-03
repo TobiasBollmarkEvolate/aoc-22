381f7e1355ce86f93a8a79c98182784cc86792d0
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-03 15:04:19 +0100

    Day 3 part1

---
+++ 3/main.py
@@ -0,0 +1,27 @@
+def get_score(char: str) -> int:
+    ascii_code = ord(char)
+    return ascii_code - 96 if ascii_code > 96 else ascii_code - 38
+
+
+def split_middle(data: str) -> tuple[str, str]:
+    middle = int(len(data) / 2)
+    return data[:middle], data[middle:]
+
+
+def find_commmon_char(string1: str, string2: str) -> str:
+    for char in string1:
+        if char in string2:
+            return char
+
+
+def main(file_name: str) -> tuple[int, int]:
+    score: int = 0
+    with open(file_name) as f:
+        for line in f:
+            compartment1, compartment2 = split_middle(line.strip('\n'))
+            score += get_score(find_commmon_char(compartment1, compartment2))
+    return score, 0
+
+
+if __name__ == '__main__':
+    print(main('input.txt'))

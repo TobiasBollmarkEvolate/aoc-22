da6b7dd72e669c40752384729a205f5be469219c
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-03 15:33:48 +0100

    Day 3 part2

--- 3/main.py
+++ 3/main.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def get_score(char: str) -> int:
     ascii_code = ord(char)
     return ascii_code - 96 if ascii_code > 96 else ascii_code - 38
@@ -8,19 +11,37 @@ def split_middle(data: str) -> tuple[str, str]:
     return data[:middle], data[middle:]
 
 
-def find_commmon_char(string1: str, string2: str) -> str:
+def find_common_char(string1: str, string2: str) -> str:
     for char in string1:
         if char in string2:
             return char
 
 
+def find_common_char_iterativly(strings: list[str], chars: list[str] = None):
+    if chars is None:
+        chars = strings.pop(0)
+    for char in chars:
+        if char in strings[0]:
+            strings.pop(0)
+            if not strings:
+                return char
+            char = find_common_char_iterativly(strings, [char])
+            if char:
+                return char
+    return False
+
+
 def main(file_name: str) -> tuple[int, int]:
-    score: int = 0
+    score1: int = 0
+    score2: int = 0
     with open(file_name) as f:
         for line in f:
             compartment1, compartment2 = split_middle(line.strip('\n'))
-            score += get_score(find_commmon_char(compartment1, compartment2))
-    return score, 0
+            score1 += get_score(find_common_char_iterativly([compartment1, compartment2]))
+    with open(file_name) as f:
+        for line1, line2, line3 in itertools.zip_longest(*[f] * 3):
+            score2 += get_score(find_common_char_iterativly([line1, line2, line3]))
+    return score1, score2
 
 
 if __name__ == '__main__':

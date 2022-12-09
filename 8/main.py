e6cf4c98c0f595929d42f06805652677144129ba
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-09 17:12:24 +0100

    day 8 part 2

--- 8/main.py
+++ 8/main.py
@@ -21,7 +21,7 @@ def visible_from_any_side(matrix: list[list[int]]):
     visible -= 4  # Don't count the corners twice
     for i, line in enumerate(matrix[1:-1]):  # removed first and last rows
         for j in range(1, len(line) - 1):
-            # Check this tree if its NOT visiable from:
+            # Check this tree if its NOT visible from:
             # right and left
             if check_row(line, j):  # Right and left
                 visible += 1
@@ -31,12 +31,45 @@ def visible_from_any_side(matrix: list[list[int]]):
     return visible
 
 
+def view_distance(i, row):
+    ret = 0
+    for tree in row:
+        ret += 1
+        if tree >= i:
+            break
+    return ret
+
+
+def scenic_score_row(row, position):
+    check_tree = row[position]
+    left_view = row[:position]
+    if len(left_view) > 1:
+        left_view.reverse()
+    left = view_distance(check_tree, left_view)
+    if left == 0:
+        return 0
+    right = view_distance(check_tree, row[position + 1:])
+    return left * right
+
+
+def scenic_max_score(matrix: list[list[int]]):
+    max_score = 0
+    for i, line in enumerate(matrix[1:-1]):  # removed first and last rows
+        for j in range(1, len(line) - 1):
+            score1 = scenic_score_row(line, j)
+            score2 = scenic_score_row([x[j] for x in matrix], i + 1)
+            score = score1 * score2
+            if score > max_score:
+                max_score = score
+    return max_score
+
+
 def main(file_name: str) -> tuple[int, int]:
     tree_matrix: list[list[int]] = []
     with open(file_name) as f:
         for line in f:
             tree_matrix.append([int(x) for x in list(line.strip())])
-    return visible_from_any_side(tree_matrix), 0
+    return visible_from_any_side(tree_matrix), scenic_max_score(tree_matrix)
 
 
 if __name__ == '__main__':

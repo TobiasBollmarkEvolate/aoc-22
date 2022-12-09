f85be7c4cc2bce66912810d920e074b555b0f09c
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-09 16:28:44 +0100

    Type hinting

--- 7/main.py
+++ 7/main.py
@@ -4,11 +4,11 @@ import typing
 
 
 class FileSystemObjectHolder:
-    def __init__(self):
+    def __init__(self) -> None:
         self.content: list[Directory | File] = []
         self.size: int = 0
 
-    def add_content(self, content: FileSystemObjectHolder | File):
+    def add_content(self, content: FileSystemObjectHolder | File) -> None:
         for obj in self.content:
             if obj.name == content.name:
                 raise Exception(f"File or folder with name {content.name} already exists.")
@@ -17,44 +17,44 @@ class FileSystemObjectHolder:
         if hasattr(content, "size"):
             self.change_size(content.size)
 
-    def change_size(self, change: int):
+    def change_size(self, change: int) -> None:
         self.size += change
         if hasattr(self, "parent"):
             self.parent.change_size(change)
 
 
 class FileSystemRoot(FileSystemObjectHolder):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.name = "/"
 
 
 class FileSystemObject:
-    def __init__(self, name: str, parent: FileSystemObjectHolder):
+    def __init__(self, name: str, parent: FileSystemObjectHolder) -> None:
         self.name: str = name
         self.parent: FileSystemObjectHolder = parent
 
 
 class Directory(FileSystemObject, FileSystemObjectHolder):
-    def __init__(self, name: str, parent: FileSystemObjectHolder):
+    def __init__(self, name: str, parent: FileSystemObjectHolder) -> None:
         FileSystemObject.__init__(self, name, parent)
         FileSystemObjectHolder.__init__(self)
 
 
 class File(FileSystemObject):
-    def __init__(self, name: str, parent: FileSystemObjectHolder, size: int):
+    def __init__(self, name: str, parent: FileSystemObjectHolder, size: int) -> None:
         super().__init__(name, parent)
         if size is not None:
             self.size = size
 
 
 class FileSystem:
-    def __init__(self, size: int = 0):
-        self._root = FileSystemRoot()
-        self._cwd: [FileSystemRoot | Directory] = self._root
+    def __init__(self, size: int = 0) -> None:
+        self._root: FileSystemRoot = FileSystemRoot()
+        self._cwd: FileSystemObjectHolder = self._root
         self.total: int = size
 
-    def cd(self, d: str):
+    def cd(self, d: str) -> None:
         if d == "/":
             self._cwd = self._root
         elif d == "..":
@@ -64,7 +64,7 @@ class FileSystem:
                 if obj.name == d:
                     self._cwd = obj
 
-    def cwd(self):
+    def cwd(self) -> FileSystemObjectHolder:
         return self._cwd
 
     def find(self, d: typing.Optional[FileSystemObjectHolder], dirs_only: bool = True) -> list[Directory | File]:
@@ -80,22 +80,22 @@ class FileSystem:
                     ret.append(obj)
         return ret
 
-    def free(self):
+    def free(self) -> int:
         return self.total - self._root.size
 
 
-def process_ls(file_system: FileSystem, f: typing.TextIO):
+def process_ls(file_system: FileSystem, f: typing.TextIO) -> None:
     while True:
         pos: int = f.tell()
         line: str = f.readline()
         if line and line[0] != "$":
             words: list = line.split()
-            cwd = file_system.cwd()
+            cwd: FileSystemObjectHolder = file_system.cwd()
             if words[0] == "dir":
                 cwd.add_content(Directory(words[1], cwd))
             else:
                 size: int = int(words[0])
-                filename = words[1]
+                filename: str = words[1]
                 cwd.add_content(File(filename, cwd, size))
         else:
             f.seek(pos)
@@ -103,23 +103,23 @@ def process_ls(file_system: FileSystem, f: typing.TextIO):
 
 
 def main(file_name: str) -> tuple[int, int]:
-    file_system = FileSystem(size=70000000)
+    file_system: FileSystem = FileSystem(size=70000000)
     with open(file_name) as f:
         while True:
-            line = f.readline()
+            line: str = f.readline()
             if not line:
                 break
             line = line.strip()
             if line[0] == "$":
-                args = line[2:].split(" ", 1)
-                cmd = args.pop(0)
+                args: list = line[2:].split(" ", 1)
+                cmd: str = args.pop(0)
                 if cmd == "cd":
                     file_system.cd(args[0])
                 elif cmd == "ls":
                     process_ls(file_system, f)
     part1: int = 0
     file_system.cd("/")
-    free_space = file_system.free()
+    free_space: int = file_system.free()
     part2: int = file_system.total
     for d in file_system.find(d=None, dirs_only=True):
         if d.size < 100000:

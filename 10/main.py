786e4247657c7d472cc7d1d8508ff9e6e0c83966
Tobias Bollmark <tobias.bollmark@evolate.com>
2022-12-12 16:11:14 +0100

    day 10 part 2 - getting a bit lazy :P

--- 10/main.py
+++ 10/main.py
@@ -1,3 +1,27 @@
+class CRT:
+    def __init__(self, size: tuple[int, int]):
+        self._pixels: list[bool] = []
+        self._width = size[0]
+        self._height = size[1]
+        self._pos = 0
+        self._row = 0
+        for _ in range(self._height * self._width):
+            self._pixels.append(False)
+
+    def draw(self, value):
+        if self._pos % self._width == 0:
+            print("")
+            self._row += 1
+            self._pos = 0
+        if value in [self._pos - 1, self._pos, self._pos + 1]:
+            print("#", end="")
+        else:
+            print(".", end="")
+        self._pos += 1
+        if self._pos > len(self._pixels):
+            self._row = 0
+
+
 class Clock:
     def __init__(self, monitors: list[int]):
         self._tick: int = 0
@@ -18,8 +42,9 @@ class Clock:
         return ret
 
 
-def main(file_name: str) -> tuple[int, int]:
+def main(file_name: str) -> int:
     clock = Clock([20, 60, 100, 140, 180, 220])
+    screen = CRT((40, 6))
     value = 1
     with open(file_name) as f:
         for line in f:
@@ -27,11 +52,15 @@ def main(file_name: str) -> tuple[int, int]:
             args = line.split()
             if args[0] == "noop":
                 clock.incr(value)
+                screen.draw(value)
             elif args[0] == "addx":
                 clock.incr(value)
+                screen.draw(value)
                 clock.incr(value)
+                screen.draw(value)
                 value += int(args[1])
-    return clock.monitor_sum(), 0
+    print("")
+    return clock.monitor_sum()
 
 
 if __name__ == '__main__':
